9386decc3c730d99181dcbaa91ad5c252edb4315
sahildatax <84317311+sahildatax@users.noreply.github.com>
2022-02-16 01:14:57 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -4,5 +4,5 @@ class Config:
     API_ID = 8763712
     API_HASH = "835d27216f117e22a5c192b89a4ce457"
     TOKEN = "5252637312:AAFv9R3Z1-7AYop217RhBqvDEWYY0zfXCd8"
-    DOMAIN  = os.getenv("domain","https://newdlstar.herokuapp.com")
+    DOMAIN  = "https://classesbysahil.vercel.app"
     CHANNEL = -1001631582129
